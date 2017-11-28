510011d195970f7d06cb03c9df1e91b88d97cae9
Aria <Aria@DESKTOP-64AL87U>
2017-11-28 22:14:14 +0300

    Cefsharp eklendi. Bağlam yapılmadı. Ertesi gün, bağlama yapılıp, 1 seri hazır sisteme yüklü o seriyi izlemeyi deneyeceğiz.

--- Animium/Form1.cs
+++ Animium/Form1.cs
@@ -7,14 +7,120 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using HtmlAgilityPack;
+using System.Net;
 
 namespace Animium
 {
     public partial class Form1 : Form
     {
+        public String html;
+        public Uri url;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 1; i < 14; i++)
+            {
+                VeriAl("http://www.anime1.com/watch/009-1", "//*[@id='center']/div[2]/div[2]/div[1]/ul/li[" + i + "]/a", "href", comboBox1);
+            }
+        }
+        public void VeriAl(string Url,String XPath,String dip, ComboBox CıkanSonuc)
+        {
+            try
+            {
+                url = new Uri(Url);
+            }
+            catch (UriFormatException)
+            {
+                if (MessageBox.Show("Hatalı Url)", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error) == DialogResult.OK)
+                {
+
+                }
+            }
+            catch (ArgumentNullException)
+            {
+                if (MessageBox.Show("Hatalı Url)", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error) == DialogResult.OK)
+                {
+
+                }
+            }
+
+            WebClient client = new WebClient();
+            client.Encoding = Encoding.UTF8;
+            try
+            {
+                html = client.DownloadString(url);
+            }
+            catch (WebException)
+            {
+                if (MessageBox.Show("Hatalı Url)", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error) == DialogResult.OK)
+                {
+
+                }
+            }
+
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(html);
+            try
+            {
+                CıkanSonuc.Items.Add(doc.DocumentNode.SelectSingleNode(XPath).Attributes[dip].Value);
+            }
+            catch (NullReferenceException)
+            {
+                if (MessageBox.Show("Hatalı XPath)", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error) == DialogResult.OK)
+                {
+
+                }
+            }
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = comboBox1.SelectedItem.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
         }
     }
 }
